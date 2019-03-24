ad6133803a956c0f5c870620834c16306a5203a2
Kevin Liu <kdliu00@gmail.com>
2019-03-23 18:03:48 -0700

    File upload initial

---
+++ Assets/Scripts/PlaneControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlaneControl : MonoBehaviour
+{
+    public float throttleCoeff;
+    public float turnSpeed;
+    public float dragCoeff;
+
+    private Rigidbody rb;
+    private bool throttle;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        throttle = false;
+    }
+    
+    void Update() {
+        transform.Rotate(Input.GetAxis("Vertical") * turnSpeed * Time.deltaTime, 
+                            Input.GetAxis("Rudder") * turnSpeed * Time.deltaTime * 0.2f, 
+                            -Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime);
+                    
+        throttle = Input.GetKey(KeyCode.Space);
+    }
+
+    void FixedUpdate() {
+        print(rb.velocity.magnitude);
+
+        if (throttle) {
+            rb.AddForce(transform.forward * throttleCoeff);
+        }
+
+        rb.drag = -0.95f + Mathf.Pow(dragCoeff, rb.velocity.magnitude);
+    }
+
+}
