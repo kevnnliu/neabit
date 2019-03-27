4023e4bb6e089f6cb61455125d1448b831c2d919
Dhruba Ghosh <djghosh13@gmail.com>
2019-03-27 10:14:57 -0700

    Changed to spherical movement

--- Assets/Scripts/SPPlaneControl.cs
+++ Assets/Scripts/SPPlaneControl.cs
@@ -9,22 +9,29 @@ public class SPPlaneControl : MonoBehaviour
     private Rigidbody rb;
 
     // PHYSICS values
+    public Vector3 sphericalCoords;
 
     // SIMULATED CONTROLLER (Will be actual controller in VR)
     private float cyclicPositionX = 0;
     private float cyclicPositionY = 0;
     private float collectivePosition = 0;
 
+    private Vector3 velocity = Vector3.zero;
+
     // How much keyboard inputs affect controller (will not be necessary in VR)
     public float controlRate = 2;
-    public float turnRate = 2;
+    public float turnRate = 30;
 
-    // Pitch parameters (Increasing/Decreasing altitude)
+    // Control parameters
+    public float cyclicPower = 10;
+    public float collectivePower = 10;
+    public float maximumVelocity = 10;
 
     void Start()
     {
         pilotCamera.SetActive(true);
         rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
     }
 
     void Update()
@@ -34,14 +41,43 @@ public class SPPlaneControl : MonoBehaviour
         cyclicPositionY = ShiftTowards(cyclicPositionY, Input.GetAxis("Pitch"), controlRate);
         collectivePosition = ShiftTowards(collectivePosition, Input.GetAxis("Throttle"), controlRate);
 
-        transform.position += transform.forward * collectivePosition;
+        Vector3 accel = Vector3.forward * collectivePosition * maximumVelocity;
+        velocity = Vector3.Lerp(velocity, accel, 0.05f);
+        // Update physics manually
+        ApplyVelocity(velocity);
 
-        // 
+        // Reset view to flat
         Vector3 targetUp = transform.position - planet.position;
         Vector3 targetForward = Vector3.ProjectOnPlane(transform.forward, targetUp).normalized;
-
         Quaternion targetRotation = Quaternion.LookRotation(targetForward, targetUp);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.1f);
+        transform.rotation = ShiftTowards(transform.rotation, targetRotation, turnRate);
+
+        sphericalCoords = ToSpherical(velocity);
+    }
+
+    // Physics stuff
+    void ApplyVelocity(Vector3 velocity)
+    {
+        Vector3 up = transform.position.normalized;
+        Vector3 right = Vector3.Cross(transform.forward, up).normalized;
+        Vector3 forward = Vector3.Cross(up, right);
+
+        Vector3 newPos = transform.position + right * velocity.x + forward * velocity.z;
+        newPos = newPos.normalized * transform.position.magnitude;
+        transform.position = newPos + up * velocity.y;
+    }
+    
+    Vector3 ApplyAcceleration(Vector3 velocity, Vector3 accel, float maxVelocity)
+    {
+        float v = velocity.magnitude / maxVelocity;
+        Vector3 dV = accel * Time.deltaTime;
+        Vector3 target = velocity + dV;
+        float vtarget = target.magnitude;
+        if (vtarget > maxVelocity)
+        {
+            return target * maxVelocity / vtarget;
+        }
+        return target;
     }
 
     // Updates a value towards a target value, with a maximum delta per second
@@ -52,5 +88,45 @@ public class SPPlaneControl : MonoBehaviour
             return Mathf.Min(value + Mathf.Abs(delta) * Time.deltaTime, target);
         }
         return Mathf.Max(value - Mathf.Abs(delta) * Time.deltaTime, target);
+    }
+
+    Vector3 ShiftTowards(Vector3 value, Vector3 target, float delta)
+    {
+        Vector3 diff = target - value;
+        float dist = diff.magnitude;
+        if (dist < delta * Time.deltaTime)
+        {
+            return target;
+        }
+        return value + diff / dist * delta * Time.deltaTime;
+    }
+
+    Quaternion ShiftTowards(Quaternion value, Quaternion target, float delta)
+    {
+        if (Quaternion.Angle(value, target) < delta * Time.deltaTime)
+        {
+            return target;
+        }
+        return Quaternion.Slerp(value, target, delta * Time.deltaTime / Quaternion.Angle(value, target));
+    }
+
+    // Converts spherical coordinates to Cartesian coordinates
+    Vector3 ToCartesian(Vector3 vector)
+    {
+        return new Vector3(
+            Mathf.Sin(vector.y) * Mathf.Cos(vector.z),
+            Mathf.Sin(vector.y) * Mathf.Sin(vector.z),
+            Mathf.Cos(vector.y)
+            ) * vector.x;
+    }
+
+    Vector3 ToSpherical(Vector3 vector)
+    {
+        float r = vector.magnitude;
+        return new Vector3(
+            r,
+            Mathf.Acos(vector.y / r),
+            Mathf.Atan2(vector.z, vector.x)
+            );
     }
 }
